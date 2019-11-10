79ded19b004ebad87adc6bbbdb81f3730d5bfc14
Moritz Lange <moritz.lange@stud.hs-hannover.de>
2019-11-10 22:33:32 +0100

    initial

---
+++ src/main/java/de/hsh/vis/buildserverdemo/BuildserverdemoApplication.java
@@ -0,0 +1,21 @@
+package de.hsh.vis.buildserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BuildserverdemoApplication {
+
+	@RequestMapping("/")
+	public String helloWorld(){
+		return "Hello Buildservers!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildserverdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsh/vis/buildserverdemo/BuildserverdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsh.vis.buildserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildserverdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
